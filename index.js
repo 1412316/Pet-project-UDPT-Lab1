d0ff3966ca7d525b2788ddaff34963000c13332c
1412316 <nhatminhdang1996@gmail.com>
2020-05-13 16:15:20 +0700

    Responsive side menu

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const toggleButton = document.getElementsByClassName('hamburger')[0];
+// Button toggle nav-bar
+const toggleButton = document.getElementsByClassName('nav__hamburger')[0];
 const navbarLinks = document.getElementsByClassName('nav-bar')[0];
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
+});
+
+// Button toggle menu
+const toggleButtonMenu = document.getElementsByClassName('menu-expand')[0];
+const menuLinks = document.getElementsByClassName('menu__block')[0];
+
+toggleButtonMenu.addEventListener('click', () => {
+  menuLinks.classList.toggle('active')
 });
